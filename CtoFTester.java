df8ea9ad7a4b3a3816a7db983eca6395f62bcf07
Sophia Gao <sgao20@stuy.edu>
2021-09-23 19:07:24 -0400

    added variables and comments

--- CtoFTester.java
+++ CtoFTester.java
@@ -9,7 +9,20 @@ import java.util.Scanner;
 
 public class CtoFTester.java{
   public static void main(String[] args){
-    celsiusToFahrenheit();
-    fahrenheitToCelsius();
+    double inCelsius;
+    double inFahrenheit;
+    //these two are the inputs for the functions
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is the temperature in celsius? ");
+    inCelsius = in.nextDouble();
+    System.out.print("What is the temperature in fahrenheit? ");
+    inFahrenheit = in.nextDouble();
+    /*
+    wouldn't need to include in codingbat
+    but opening in terminal requires some value for variable be coded
+    this allows the user to test various values for variables
+    */
+    celsiusToFahrenheit(inCelsius);
+    fahrenheitToCelsius(inFahrenheit);
   }
 }
