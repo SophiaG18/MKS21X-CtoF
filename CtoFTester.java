a2d8e5605a1a778d871e20b5e739c8b2c18c47d6
Sophia Gao <sgao20@stuy.edu>
2021-09-23 19:00:37 -0400

    added the functions and comments

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,15 @@
+/*
+The functions should take a double for the parameter
+The celsiusToFahrenheit should take the celsius temperature
+The fahrenheitToCelsius should take the fahrenheit temperature
+The functions should also be a double (returns double)
+*/
+
 import java.util.Scanner;
 
 public class CtoFTester.java{
   public static void main(String[] args){
-
+    celsiusToFahrenheit();
+    fahrenheitToCelsius();
   }
 }
